e8e4409b9d8856bb76c247689d380359f8c9ee7e
gianni <gianni.pele92@gmail.com>
2017-12-13 21:40:11 +0100

    changes before start adding the collisions part

--- mvc/model/weapons.py
+++ mvc/model/weapons.py
@@ -10,17 +10,25 @@ class Weapon:
     """
 
     def __init__(self):
+<<<<<<< d4107b6dd6f1de37ec47cb5450f5bfe21467d885
         self.power = 3
         self.ratio = 0.5
         self.last_shot = time.time()
         self.precision = 16
+=======
+        self.power = 200
+        self.damage = 10
+        self.ratio = 0.05
+        self.last_shot = time.time()
+        self.precision = 20
+>>>>>>> changes before start adding the collisions part
 
     def shoot(self, pid, angle, pos):
         now = time.time()
         if now - self.last_shot > self.ratio:
             self.last_shot = now
             direction = _compute_randrange_direction(self.precision, angle)
-            return Bullet(pid, pos.x, pos.y, direction, 30, self.power)
+            return Bullet(pid, pos.x, pos.y, direction, 20, self.damage, self.power)
 
 
 def _compute_randrange_direction(precision, angle):
@@ -35,4 +43,16 @@ def _compute_randrange_direction(precision, angle):
     elif min_angle < max_angle:
         rand_angle = rand.uniform(min_angle, max_angle)
 
+<<<<<<< d4107b6dd6f1de37ec47cb5450f5bfe21467d885
     return ggutilities.angle_to_direction(rand_angle)
+=======
+    return _angle_to_direction(rand_angle)
+
+
+# Convert the angle to the x,y direction
+def _angle_to_direction(angle):
+    radians = angle * math.pi / 180
+    vx = math.cos(radians)
+    vy = math.sin(radians)
+    return vec2d((vx, vy)).normalized()
+>>>>>>> changes before start adding the collisions part
